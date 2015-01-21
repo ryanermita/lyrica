57ba04b076dca0116f83dcc2ebdaf7faac3a2b1a
Ryan Jayson Ermita <ryanermita@gmail.com>
2015-01-21 19:49:28 +0800

    add simple documentation for crud helper.

--- lyrica/helpers/helper.py
+++ lyrica/helpers/helper.py
@@ -4,6 +4,10 @@ from bson.objectid import ObjectId
 import ast
 
 
+# Parse mongo object to more 
+# readable list of dict.
+# Remove _id field and moved its
+# value to id key.
 def parse_mongoObj(jsn):
     jsn =ast.literal_eval(dumps(jsn))
     for i, v in enumerate(jsn):
@@ -12,11 +16,14 @@ def parse_mongoObj(jsn):
 
     return jsn
 
+# For single data. convert list to 
+# dictionary.
 def to_dict(jsn):
     result = {}
     for d in jsn: result.update(d)
     return result
 
+# Insert new lyrica, return newly added record.
 def insert_lyrica(data):
     db[collection].insert(data)
     lyrica = db[collection].find(data)
@@ -24,9 +31,12 @@ def insert_lyrica(data):
 
     return to_dict(r)
 
+# delete lyrica based on its id
 def delete_lyrica(id):
     db[collection].remove({'_id': ObjectId(id)})
 
+# update lyrica based on its id
+# and return newly updated record.
 def update_lyrica(id, data):
     db[collection].update({'_id': ObjectId(id)}, {'$set': data})
     lyrica = db[collection].find({'_id': ObjectId(id)})
@@ -34,6 +44,8 @@ def update_lyrica(id, data):
 
     return to_dict(r)
 
+# Get record in db, if id is present fetch single record.
+# if id is None fetch all record.
 def get_lyrica(id):
     lyrica = db[collection].find({'_id': ObjectId(id)})
     mongoObj = parse_mongoObj(lyrica)
@@ -42,6 +54,5 @@ def get_lyrica(id):
     if id is None:
         lyrica = db[collection].find()
         r = parse_mongoObj(lyrica)
-
 
     return r
